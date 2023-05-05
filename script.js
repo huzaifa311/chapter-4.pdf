75a343dba676893981fade61b4203875a1670b84
huzaifa311 <119949757+huzaifa311@users.noreply.github.com>
2023-05-05 21:22:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+//                                  VARIABLE NAMES: LEGAL & ILLEGAL 
+
+/*Question no 01: Declare 3 variables in one statement.*/
+var x = 10, y = "hello", z = true;
+console.log(x , y , z);
+
+/*Question no 02: Declare 5 legal & 5 illegal variable names.*/
+document.write("<h1>Legal variable names:</h1>")
+document.write("<p>1. firstName</p>")
+document.write("<p>2. age1</p>")
+document.write("<p>3. $price</p>")
+document.write("<p>4. camelCase</p>")
+document.write("<p>5. _underscore</p>")
+
+document.write("<h1>Illegal variable names:</h1>")
+document.write("<p>1. 1age</p>")
+document.write("<p>2. full name</p>")
+document.write("<p>3. #salary</p>")
+document.write("<p>4. break</p>")
+document.write("<p>5. my-name</p>")
+
+/*Question no 03: Display this in your browser
+a) A heading stating “Rules for naming JS variables”
+b) Variable names can only contain ______, ______, ______ and ______.
+For example $my_1stVariable
+c) Variables must begin with a ______, ______ or _____. For example $name, _name or name
+d) Variable names are case _________
+e) Variable names should not be JS _________*/
+document.write("<h1>Rules for naming JS variables</h1>")
+document.write("<p>Variable names can only contain letters, numbers, $, and _. For example: $my_1stVariable</p>")
+document.write("<p>Variables must begin with a letter, $, or _. For example: $name, _name, or name</p>")
+document.write("<p>Variable names are case sensitive</p>")
+document.write("<p>Variable names should not be JS keywords</p>")
+
+
+
+
+
+
+
+
